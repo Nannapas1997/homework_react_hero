cd7e62e9f7d05927291e62df152c7da3ef62663a
Nannapas <nantapat1997@gmail.com>
2024-02-26 12:45:40 +0700

    Third commit

--- homework7_2.js
+++ homework7_2.js
@@ -1,3 +1,12 @@
-axios.get('homework1-4.json').then(response => {
-    console.log(response.data);
+
+
+axios.get('https://dev.codekit.co/devcamp-api/covid-country.txt').then(response => {
+    const allCovidData = response.data
+    console.log(allCovidData.reduce((sum, obj) => sum + obj["todayRecovered"], 0))
+    console.log(allCovidData.filter((country) => country["population"] > 100000000 && country["todayDeaths"] < 10)
+                                .map((country) => country["country"]))
+
 });
+
+    
+
